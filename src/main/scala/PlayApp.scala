336533177390a3310e49f2a7df1661ca50ac2d0e
CMonkey <42.codemonkey@gmail.com>
2020-03-28 16:10:20 +0800

    format code

--- src/main/scala/PlayApp.scala
+++ src/main/scala/PlayApp.scala
@@ -2,22 +2,22 @@ import java.lang.reflect.{AccessibleObject, Field}
 
 class PlayApp extends App{
 
-  val addressExtractor = new AddressExtractor()
+  val addressExtractor: AddressExtractor = new AddressExtractor()
   val clazz:Class[_ <:AddressExtractor] = addressExtractor.getClass
-  val field = clazz.getDeclaredField("pointerValue")
-  val fieldType = classOf[Field].getDeclaredField("type")
+  val field: Field = clazz.getDeclaredField("pointerValue")
+  val fieldType: Field = Predef.classOf[Field].getDeclaredField("type")
 
-  AccessibleObject.setAccessible(Array(field, fieldType), true)
+  AccessibleObject.setAccessible(Array.apply(field, fieldType), true)
 
-  fieldType.set(field, classOf[Object])
+  fieldType.set(field, Predef.classOf[Object])
 
-  val dummy = new Dummy()
+  val dummy: Dummy = new Dummy()
   dummy.value = 0xBADF00D
 
   field.set(addressExtractor, dummy)
 
   System.out.println(java.lang.Long.toHexString(addressExtractor.pointerValue))
-  System.out.println(field.get(addressExtractor) == dummy)
+  System.out.println(field.get(addressExtractor).==(dummy))
 
   System.in.read()
 
