54d229bf07b0929b810103a3fc78803930e640b0
CMonkey <42.codemonkey@gmail.com>
2020-03-28 17:01:52 +0800

    add testReadingObjectContent

--- src/main/scala/org/excavator/jvm/inside/Dummy.scala
+++ src/main/scala/org/excavator/jvm/inside/Dummy.scala
@@ -1,7 +1,7 @@
 package org.excavator.jvm.inside
 
-class Dummy(var value: Long, var value2: Long , var value3:Long){
+class Dummy(var value: Long){
   def this(){
-    this(0L, 0L, 0L)
+    this(0L)
   }
 }
